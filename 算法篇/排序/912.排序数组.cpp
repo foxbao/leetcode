ef36cb0edd54a64082516526a8af282e2929fca5
foxbao <jidushanbojue1776@hotmail.com>
2024-03-05 15:45:13 +0800

    updated some cpp

---
+++ 算法篇/排序/912.排序数组.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        
+    }
+};
+
+int main()
+{
+    Solution solution;
+    vector<int> nums={5,2,3,1};
+    solution.sortArray(nums);
+    return 1;
+}
